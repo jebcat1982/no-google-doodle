82a9e4f40fcdc4cfe112e8da6ec32ad094b2e210
Atanas Roussev <atanas@nextinterfaces.com>
2016-11-17 23:18:32 -0800

    First commit, Goodbye Doodle! I won’t miss you.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# no-google-doodle
-This is a Chrome extension, that turns off annoying Google doodles and changes it with your picture of choice
+no-google-doodle
+==========
+
+This is a Chrome extension, that turns off annoying Google doodles and changes it with a blank page.
+
+# Install
+
+
+
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function () {
+//     var arr = [
+//         'https://www.target.com.au/medias/sys_master/h79/h7f/8938896261150.jpg'
+//     ];
+//     var randomNumber = Math.floor(Math.random() * arr.length);
+//     $("._img").css('background-image', 'url(' + arr[randomNumber] + ')');
+// });
+//
